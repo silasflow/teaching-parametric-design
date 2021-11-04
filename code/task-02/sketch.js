dfc2cc01ffb35fa2f4e0111694fb2288d38e6c6e
silasflow <silas.wolf@icloud.com>
2021-11-04 10:38:44 +0100

    task-01 Optimierungen

--- code/task-02/sketch.js
+++ code/task-02/sketch.js
@@ -3,11 +3,11 @@ const max = 400;
 function setup() {
   createCanvas(max, max);
   background(255);
-  // rectMode(CENTER);
+  frameRate(0.25);
 };
 
 function draw() {
-  noLoop();
+  background(255);
   let distance = 115;
   let rectSize = 60;
   stroke(0);
@@ -18,7 +18,7 @@ function draw() {
       let rNumber = random(0, 5);
 
       if (rNumber >= 4) {
-        swirl(55 + distance * x, 55 + distance * y, 100);
+        swirl(55 + distance * x, 55 + distance * y, 120);
       } else {
         if (x % 2 === 0) {
           rect(30 + distance * x, 30 + distance * y, rectSize);
@@ -39,7 +39,7 @@ function draw() {
 function swirl(xS, yS, circleR) {
   noFill();
   circle(xS, yS, circleR*0.8);
-  for (let i = 0; i <= 70; i++) {
+  for (let i = 0; i <= 80; i++) {
     circle(
       xS + random(random(-circleR / 6, -circleR / 3), random(circleR / 6, circleR / 3)),
       yS + random(random(-circleR / 6, -circleR / 3), random(circleR / 6, circleR / 3)),
