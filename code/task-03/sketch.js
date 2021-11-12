2d9ac01bfbbe64a984b3978421e4dbaef21459f2
silasflow <silas.wolf@icloud.com>
2021-11-12 09:24:56 +0100

    3

--- code/task-03/sketch.js
+++ code/task-03/sketch.js
@@ -1,5 +1,5 @@
 const max = 400,
-  dotCount = 50;
+  dotCount = 30;
 var radius = 10,
   degree = 0;
 
@@ -15,7 +15,6 @@ function setup() {
 
 function draw() {
   background(0);
-  whatTheRadius()
   var x = radius * cos(degree);
   var y = radius * sin(degree);
   degree++;
@@ -32,12 +31,14 @@ function draw() {
       x: random(0, max),
       y: random(0, max)
     })
-
   }
 
+  stroke(0, 0, 100);
+  fill("black");
+  circle(max / 2, max / 2, radius * 2);
 
   for (let i = 0; i <= dotCount; i++) {
-    stroke(0, 0, 100 / dotCount * i);
+    stroke(0, 70, 100 / dotCount * i);
     strokeWeight(5);
     point(dot[i].x, dot[i].y)
 
@@ -46,6 +47,8 @@ function draw() {
     vertex(max / 2 + x, max / 2 + y);
     endShape();
   }
+
+  circle(max / 2 + x, max / 2 + y, 5);
 
 };
 
