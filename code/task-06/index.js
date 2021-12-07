b0bf05a7599b4a8e368489b75fde7d3ef0d3d72b
silasflow <93256160+silasflow@users.noreply.github.com>
2021-12-07 18:59:19 +0100

    nochmal unter 5mb

--- code/task-06/index.js
+++ code/task-06/index.js
@@ -43,24 +43,24 @@ const {
 const main = () => {
   const shapes = [];
 
-  for (let j = 0; j <= 10; j++) {
-    for (let i = 0; i <= 10; i++) {
+  for (let j = 0; j <= 7; j++) {
+    for (let i = 0; i <= 7; i++) {
 
         shapes.push(roundedCylinder({
           roundRadius: 1,
           center: [0 + 10 * i, 0 + 10 * j, 0 + 5 * j + 5 * i],
           height: 10 + 10 * i + 10 * j,
           radius: 5,
-          segments: 20,
+          segments: 16,
         }));
 
     }
   }
 
-  const base = translate([50, 50, -10], roundedCuboid({
-    size: [120, 120, 20],
-    roundRadius: 5,
-    segments: 16,
+  const base = translate([35, 35, -10], roundedCuboid({
+    size: [90, 90, 20],
+    roundRadius: 3,
+    segments: 14,
   }));
 
 
