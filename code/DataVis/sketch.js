0b0b788898460674c6519ad436379fdd28d469ca
silasflow <93256160+silasflow@users.noreply.github.com>
2021-12-21 14:14:35 +0100

    viel zu viel

---
+++ code/DataVis/sketch.js
@@ -0,0 +1,64 @@
+const max = 400;
+let data,
+  button;
+
+function preload() {
+  data = loadJSON('statistic_id1224_arbeitslosenquote-in-deutschland-bis-2021 2.json');
+  // data = loadJSON('statistic_id273406_lebenserwartung-in-deutschland-bei-geburt-nach-geschlecht.json');
+  // data = loadJSON('statistic_id2229_mordopfer-in-deutschland-bis-2020.json');
+  // data = loadJSON('statistic_id427067_instagram-accounts-mit-den-meisten-followern-weltweit-im-november-2021.json');
+  // data = loadJSON('statistic_id1365_bevoelkerung-deutschlands-nach-relevanten-altersgruppen-2020.json');
+  // data = loadJSON('statistic_id2112_veraenderung-des-bruttoinlandsprodukts-im-vergleich-zum-vorjahr-bis-2020.json');
+  // data = loadJSON('statistic_id5573_niederschlagsmenge-in-deutschland-bis-november-2021.json');
+
+
+}
+
+function setup() {
+  createCanvas(max, max, SVG);
+  colorMode(HSB);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  noLoop();
+  button = createButton('SAVE');
+  button.position(0, 400);
+  button.mousePressed(saveAll);
+};
+
+function draw() {
+  translate(200, 200);
+  // let table = data.values();
+  // const table = [32, 28, 23, 18, 25, 20];
+  data = Object.values(data);
+  console.log(data);
+  noFill();
+  f = 12;
+  index = 1;
+
+  beginShape();
+  for (let j = 0; j < data.length; j++) {
+    var i = j * (360 / data.length);
+    curveVertex(polarX(data[j][index] * f, i), polarY(data[j][index] * f, i));
+    console.log(i);
+    console.log(j);
+  };
+  curveVertex(polarX(data[0][index] * f, 0), polarY(data[0][index] * f, 0));
+  curveVertex(polarX(data[1][index] * f, 1 * (360 / data.length)), polarY(data[1][index] * f, 1 * (360 / data.length)));
+  curveVertex(polarX(data[2][index] * f, 2 * (360 / data.length)), polarY(data[2][index] * f, 2 * (360 / data.length)));
+  endShape();
+
+};
+
+function polarX(radius, degree) {
+  let x = radius * cos(degree);
+  return x;
+}
+
+function polarY(radius, degree) {
+  let y = radius * sin(degree);
+  return y;
+}
+
+function saveAll (){
+  save();
+}
