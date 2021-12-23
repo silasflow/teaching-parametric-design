b1290124a8a91760082423fa2da2b7772ee88dfb
silasflow <93256160+silasflow@users.noreply.github.com>
2021-12-23 15:14:26 +0100

    Data Vis

--- code/DataVis/sketch.js
+++ code/DataVis/sketch.js
@@ -5,11 +5,11 @@ let data,
 function preload() {
   // data = loadJSON('statistic_id1224_arbeitslosenquote-in-deutschland-bis-2021 2.json');
   // data = loadJSON('statistic_id273406_lebenserwartung-in-deutschland-bei-geburt-nach-geschlecht.json');
-  // data = loadJSON('statistic_id2229_mordopfer-in-deutschland-bis-2020.json');
+  data = loadJSON('statistic_id2229_mordopfer-in-deutschland-bis-2020.json');
   // data = loadJSON('statistic_id427067_instagram-accounts-mit-den-meisten-followern-weltweit-im-november-2021.json');
   // data = loadJSON('statistic_id1365_bevoelkerung-deutschlands-nach-relevanten-altersgruppen-2020.json');
   // data = loadJSON('statistic_id2112_veraenderung-des-bruttoinlandsprodukts-im-vergleich-zum-vorjahr-bis-2020.json');
-  data = loadJSON('statistic_id5573_niederschlagsmenge-in-deutschland-bis-november-2021.json');
+  // data = loadJSON('statistic_id5573_niederschlagsmenge-in-deutschland-bis-november-2021.json');
 
 
 }
@@ -27,21 +27,32 @@ function setup() {
 
 function draw() {
   translate(200, 200);
-  beginShape();
-  vertex(-2,-2);
-  vertex(2,2);
-  endShape();
-  beginShape();
-  vertex(2,-2);
-  vertex(-2,2);
-  endShape();
+  // beginShape();
+  // vertex(-2, -2);
+  // vertex(2, 2);
+  // endShape();
+  // beginShape();
+  // vertex(2, -2);
+  // vertex(-2, 2);
+  // endShape();
   // let table = data.values();
   // const table = [32, 28, 23, 18, 25, 20];
   data = Object.values(data);
   console.log(data);
   noFill();
-  f = 1.3;
+  let f = 0.3;
   index = 1;
+  let z = data.length;
+
+
+  // beginShape();
+  // curveVertex(0 * 10 - 100, data[0][index] * f -100);
+  // for (let j = 0; j < data.length; j++) {
+  //   curveVertex(j * 10 - 100, data[j][index] * f -100);
+  //   circle(j * 10 - 100, data[j][index] * f -100,1);
+  // };
+  // curveVertex(z * 10 - 100, data[0][index] * f - 100);
+  // endShape();
 
   beginShape();
   for (let j = 0; j < data.length; j++) {
@@ -67,6 +78,6 @@ function polarY(radius, degree) {
   return y;
 }
 
-function saveAll (){
+function saveAll() {
   save();
 }
--- code/EndProjekt/sketch.js
+++ code/EndProjekt/sketch.js
@@ -1,7 +1,8 @@
 const max = 400;
 var randomStart = Math.random() * 360; // ändert den Startpunkt der Kugeln um mehr Abwechselung zu bieten
 let slider;
-var fireworkRadius = 1; // Radius der einzellene Kugeln
+var fireworkRadius = 0; // Radius der einzellene Kugeln
+
 
 function preload() {
   data = loadJSON('Umsatz_Import_Export.json'); // Daten werden geladen
@@ -13,10 +14,10 @@ function setup() {
   colorMode(HSB);
   angleMode(DEGREES);
   rectMode(CENTER);
-  // noLoop();
+  noLoop();
 
   data = Object.values(data);
-  slider = createSlider(6, data.length-1, 6, 1);
+  slider = createSlider(6, data.length - 1, 6, 1);
   slider.position(160, 400);
   slider.style('width', '80px');
 
@@ -27,7 +28,6 @@ function setup() {
 
 function draw() {
   translate(max / 2, max / 2);
-  
 
 
   let arrayIndex = slider.value(); // überführt den Silder Wert in den Index fürs Array
@@ -37,12 +37,15 @@ function draw() {
   text(str(data[arrayIndex][0]), 200, 360);
   console.log('Jahreszahl: ' + data[arrayIndex][0]);
   console.log('Tabellenlänge: ' + data.length);
-  explosion(visualisedData);
+  for (let j = 1; j <= 3; j++) {
+    explosion(visualisedData, 100);
+  };
 
   // console.log(data);
   console.log('Tabellen Input: ' + visualisedData);
-  
+
 };
+
 
 function explosion(arrayCount) {
   let segments = arrayCount / 2; // verkleinert die aus der Tabelle gezogenen Zahl
@@ -52,39 +55,47 @@ function explosion(arrayCount) {
   console.log('Kugeln im Ring: ' + ring);
   console.log('Gradzahl zwischen den Kugeln: ' + ringDegree);
 
-  noStroke();
+  let saturation = 100;
+
+  let hue = 148;
   background(100);
-  // ---- erzeugt einen Ring uas Kugeln ----
-  for (let i = 0 + randomStart; i <= 360 + randomStart; i = i + ringDegree) {
+  noStroke();
+  // saturation = saturation - (120 / 100);
+  fill(hue, saturation, 100);
 
-    // stroke(90)
-    // strokeWeight(6);
-    // noFill();
-    // circle(0, 0, max / 2);
-    // circle(0, 0, fireworkRadius - 1);
 
-    // ---- Überschreibt die Kugeln weiß ----
-    fill(100);
-    circle(polarX(max / 4, i), polarY(max / 4, i), 6);
-    circle(polarX(fireworkRadius - 1, i), polarY(fireworkRadius - 1, i), 6)
+  // ---- erzeugt einen Ring aus Kugeln ----
+  if (fireworkRadius <= 80) {
+    for (let i = 0 + randomStart; i <= 360 + randomStart; i = i + ringDegree) {
+      circle(polarX(fireworkRadius, i), polarY(fireworkRadius, i), 5)
+    };
+  }
 
-    // ---- zeichent die Kugeln ----
-    noStroke();
-    fill(0);
-    circle(polarX(fireworkRadius, i), polarY(fireworkRadius, i), 5)
-  };
+  if (fireworkRadius - 20 >= 0 && fireworkRadius <= 100) {
+    for (let i = 0 + randomStart; i <= 360 + randomStart; i = i + ringDegree) {
+      circle(polarX(fireworkRadius - 20, i), polarY(fireworkRadius - 20, i), 5)
+    };
+  }
+
+  if (fireworkRadius - 40 >= 0) {
+    for (let i = 0 + randomStart; i <= 360 + randomStart; i = i + ringDegree) {
+      circle(polarX(fireworkRadius - 40, i), polarY(fireworkRadius - 40, i), 5)
+    };
+  }
 
   // ---- testet auf maximal Radius des Ringes -----
   if (whatTheRadius(fireworkRadius) === 1) {
-    fireworkRadius = 1;
+    fireworkRadius = 0;
+    randomStart = Math.random() * 360
   } else if (whatTheRadius(fireworkRadius) === 0) {
-    fireworkRadius++;
+    fireworkRadius += 1.5;
   }
 };
 
+
 // ---- testet auf maximal Radius des Ringes -----
 function whatTheRadius(rTest) {
-  if (rTest === max / 4) {
+  if (rTest === 120) {
     i = 1;
     return i;
   } else {
